231b92c589bdcba13fe7350616cef7798509e682
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-10-10 22:02:53 +0100

    Minor refactor, reorganise code

--- gpio/src/main/scala/org/kaloz/gpio/GpioAppDI.scala
+++ gpio/src/main/scala/org/kaloz/gpio/GpioAppDI.scala
@@ -6,6 +6,6 @@ trait GpioAppDI extends GpioAppConfig {
 
   val pinController = new PinController()
 
-  val reactionController = new ReactionFlowController(pinController, reactionLedPulseLength, reactionCorrectionFactor, reactionThreshold)
-  val sessionHandler = new SessionHandler(pinController, reactionController)
+  val reactionFlowController = new ReactionFlowController(pinController, reactionLedPulseLength, reactionCorrectionFactor, reactionThreshold)
+  val sessionHandler = new SessionHandler(pinController, reactionFlowController)
 }
